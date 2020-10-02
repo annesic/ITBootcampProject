84b244682c9d209e575b595abbb0ec8fc709a5d8
annesic <neshich.aleksandra@gmail.com>
2020-10-02 11:46:46 +0200

    Modified BasicTest

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -20,38 +20,40 @@ import org.testng.annotations.BeforeClass;
 
 public abstract class BasicTest {
 
-		protected WebDriver driver;
-		protected JavascriptExecutor js;
-		protected WebDriverWait waiter;
-		protected String baseURL = "http://demo.yo-meals.com/";
-		protected String email = "customer@dummyid.com";
-		protected String password = "12345678a";
+	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait waiter;
+	protected String baseURL = "http://demo.yo-meals.com/";
+	protected String email = "customer@dummyid.com";
+	protected String password = "12345678a";
 
-		@BeforeClass
-		public void beforeClass() {
-			System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
-			this.driver = new ChromeDriver();
-			this.driver.manage().window().maximize();
-			js = (JavascriptExecutor) this.driver;
-			this.waiter = new WebDriverWait(this.driver, 20);
-			this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		}
-
-		@AfterMethod
-		public void afterMethod(ITestResult result) throws IOException {
-			if (result.getStatus() == ITestResult.FAILURE) {
-				TakesScreenshot scrShot = ((TakesScreenshot) driver);
-				File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
-				String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.png'").format(new Date());
-				File DestFile = new File("screenshots/" + fileName);
-				FileHandler.copy(SrcFile, DestFile);
-			}
-		}
-
-		@AfterClass
-		public void afterClass() {
-			this.driver.close();
-		}
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.driver.manage().window().maximize();
+		js = (JavascriptExecutor) this.driver;
+		this.waiter = new WebDriverWait(this.driver, 20);
+		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 	}
 
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			TakesScreenshot scrShot = ((TakesScreenshot) driver);
+			File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+			String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.png'").format(new Date());
+			File DestFile = new File("screenshots/" + fileName);
+			FileHandler.copy(SrcFile, DestFile);
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.navigate().refresh();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
+}
